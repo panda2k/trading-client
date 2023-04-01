cbdcf3905c6e5ac43a86381b6f3f243f6facf0bf
panda2k <thelowkeypanda@gmail.com>
2023-04-01 11:57:22 -0700

    Add typing for account balance

--- renderer/utils/ironbeam/api.ts
+++ renderer/utils/ironbeam/api.ts
@@ -1,22 +1,22 @@
-import * as mqtt from "mqtt"
-import { MqttClient } from "mqtt"
-import { CredentialConfig } from "./types"
+import * as mqtt from "mqtt";
+import { MqttClient } from "mqtt";
+import { CredentialConfig } from "./types";
 
 export default class IronbeamApi {
     /**
      * The websocket url for XAPI
      */
-    private static readonly HOST_URL = "wss://xapi.certigo.com/mqtt"
-    
+    private static readonly HOST_URL = "wss://xapi.certigo.com/mqtt";
+
     /**
      * The default operatorId
      */
-    private static readonly OPERATOR_ID = "FIRETIP"
+    private static readonly OPERATOR_ID = "FIRETIP";
 
     /**
      * The default operatorPassword
      */
-    private static readonly OPERATOR_PASSWORD = "maG@L7X3bE4y"
+    private static readonly OPERATOR_PASSWORD = "maG@L7X3bE4y";
 
     /**
      * MQTT config to send as login username
@@ -32,12 +32,12 @@ export default class IronbeamApi {
         operatorPassword: "",
         version: "2.12",
         uniqueID: ""
-    }
-    
+    };
+
     /**
      *  The mqtt client
      */
-    private client: MqttClient
+    private client: MqttClient;
 
     /**
      * Default constructor for initializing an API instance 
@@ -48,18 +48,18 @@ export default class IronbeamApi {
      * @param {string=} operatorPassword - the api operator password 
      */
     public constructor(
-        username: string = "",
-        password: string = "",
+        username = "",
+        password = "",
         operatorId: string = IronbeamApi.OPERATOR_ID,
         operatorPassword: string = IronbeamApi.OPERATOR_PASSWORD,
     ) {
-        this.setUsername(username)
-        this.config.password = password
-        this.config.operatorID = operatorId
-        this.config.operatorPassword = operatorPassword
-        this.client = mqtt.connect("")
+        this.setUsername(username);
+        this.config.password = password;
+        this.config.operatorID = operatorId;
+        this.config.operatorPassword = operatorPassword;
+        this.client = mqtt.connect("");
     }
-    
+
     /** 
      * Custom on connect method  
      * @name onConnect
@@ -71,21 +71,21 @@ export default class IronbeamApi {
      *  
      * @param {onConnect} onConnect - the function to call after connect
      */
-    public connect(onConnect: () => any): void {
+    public connect(onConnect: () => void): void {
         this.client = mqtt.connect(IronbeamApi.HOST_URL, {
             username: JSON.stringify(this.config),
             clientId: this.config.uniqueID,
-            protocolId: 'MQIsdp',
+            protocolId: "MQIsdp",
             protocolVersion: 3
-        })
+        });
 
-        this.client.on('connect', () => {
-            console.log("Connected")
-            this.client.subscribe(`CLIENT/${this.config.uniqueID}`)
-            onConnect()
-        })
+        this.client.on("connect", () => {
+            console.log("Connected");
+            this.client.subscribe(`CLIENT/${this.config.uniqueID}`);
+            onConnect();
+        });
     }
-    
+
     /**
      * Sets the config's username
      *
@@ -95,8 +95,8 @@ export default class IronbeamApi {
         this.config.username = username;
 
         // stolen from IronBeam API implementation
-        this.config.uniqueID = 
-            `${this.config.username}_${Math.random().toString(16).substr(2, 8)}`
+        this.config.uniqueID = `${this.config.username}_` +
+            Math.random().toString(16).substr(2, 8);
     }
 
 
@@ -106,7 +106,7 @@ export default class IronbeamApi {
      * @param {string} password - the password
      */
     public setPassword(password: string): void {
-        this.config.password = password
+        this.config.password = password;
     }
 
     /**
@@ -115,7 +115,7 @@ export default class IronbeamApi {
      * @returns {string} the username 
      */
     public getUsername(): string {
-        return this.config.username
+        return this.config.username;
     }
 
     /**
@@ -124,9 +124,9 @@ export default class IronbeamApi {
      * @returns {boolean} whether the client is connected
      */
     public isConnected(): boolean {
-        return this.client?.connected
+        return this.client?.connected;
     }
 }
 
-export const instance = new IronbeamApi()
+export const instance = new IronbeamApi();
 
--- renderer/utils/ironbeam/types.ts
+++ renderer/utils/ironbeam/types.ts
@@ -1,3 +1,8 @@
+import { CurrencyCode, RegCode } from "./enums";
+
+/**
+ * The credentials required to authenticate to XAPI
+ */
 export type CredentialConfig = {
     /**
      * The account number 
@@ -50,6 +55,168 @@ export type CredentialConfig = {
      * Client id
      */
     uniqueID: string
+}
+
+/**
+ * The account balance object 
+ */
+export type AccountBalance = {
+    /**
+     * The account sub account name
+     */
+    ACCOUNT: string
+
+    /**
+     * The currency being reported in
+     */
+    CURRENCY_CODE: CurrencyCode
+
+    /**
+     * The account regulation code
+     */
+    REG_CODE: RegCode
+
+    /**
+     * The account cash balance 
+     */
+    CASH_BALANCE: string
+
+    /**
+     * The open pnl
+     */
+    OPEN_TRADE_EQUITY: string
+
+    /**
+     * The current value of the account including all open asset pnls
+     */
+    NET_LIQUIDITY: string
+
+    /**
+     * The cash balance + open trade equity
+     */
+    TOTAL_EQUITY: string
+
+    /**
+     * The commisions paid today (estimate)
+     */
+    COMMISSIONS: string
+
+
+    /**
+     * The amount of fees paid today (estimate)
+     */
+    FEES: string
+
+    /**
+     * The number of days where the account went below required maintenance 
+     * margin and has yet to return above it 
+     */
+    DAYS_ON_CALL: number
+
+    /**
+     * The span margin amount, aka the amount of margin acharged to the account
+     */
+    MARGIN_INFO: MarginInfo
+
+    /**
+     * For subaccounts with a margin schedule assigned, it is the margin 
+     * calculated from the schedule. If there is no margin schedule, it is 
+     * missing 
+     */
+    MARGIN_INFO_SIMPLE?: MarginInfo
+}
+
+/**
+ * Margin info
+ */
+export type MarginInfo = {
+    /**
+     * The account sub account name
+     */
+    ACCOUNT: string
+
+    /**
+     * The currency being reported in
+     */
+    CURRENCY_CODE: CurrencyCode
+
+    /**
+     * The account regulation code
+     */
+    REG_CODE: RegCode
+
+    /**
+     * The buying power for an account
+     */
+    BUYING_POWER: string
+
+    /**
+     * The margin used for open positions
+     */
+    MARGIN_O: MarginDetail
+
+    /**
+     * The worst case margin for open positions + any combination of working 
+     * orders being executed 
+     */
+    MARGIN_OW: MarginDetail
+
+    /**
+     * The worst case margin for open positions + any combination of working
+     * and incoming orders being executed
+     */
+    MARGIN_OWI: MarginDetail
+}
+
+/**
+ * A detailed object representing margin values
+ */
+export type MarginDetail = {
+    /**
+     * A margin error. "NONE" if there is none
+     */
+    MARGIN_ERROR: string
+
+    /**
+     * The symbols triggering margin errors. csv of symbols
+     */
+    ERROR_SYMBOLS: null | string
+
+    /**
+     * The initial span risk. "-876542310.0" or "-876542310" if it is not 
+     * calculated
+     */
+    INITIAL_RISK_MARGIN: string
+
+    /**
+     * The maintenance span risk. "-876542310.0" or "-876542310" if it is not 
+     * calculated 
+     */
+    MAINTENANCE_RISK_MARGIN: string
+
+    /**
+     * The initial total span risk. INITIAL_RISK_MARGIN - long option value + 
+     * short option value. "-876542310.0" or "-876542310" if it is not 
+     * calculated 
+     */
+    INITIAL_TOTAL_MARGIN: string
+
+    /**
+     * The initial total span risk. MAINTENANCE_RISK_MARGIN - long option value 
+     * + short option value. "-876542310.0" or "-876542310" if it is not 
+     * calculated 
+     */
+    MAINTENANCE_TOTAL_MARGIN: string
+
+    /**
+     * Whether the values are estimated
+     */
+    IS_ESTIMATED: boolean
+
+    /**
+     * The time when margin was most recently calculated. 0 if uninitialized
+     */
+    AS_OF_TIME: number
 }
 
 
